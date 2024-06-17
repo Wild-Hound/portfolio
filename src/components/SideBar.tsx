d40f38532626b81cf5ff91002cb58351f81b869e
Wild-Hound <wildhound404@gmail.com>
2024-06-18 03:25:57 +0600

    temp commit

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -20,12 +20,19 @@ const SideBar: React.FC<Props> = ({ children }) => {
             aria-label="close sidebar"
             className="drawer-overlay"
           ></label>
-          <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+
+          <div className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
             {/* Sidebar content here */}
-            <li>
-              <Link href="/newpage">New Page</Link>
-            </li>
-          </ul>
+            <div className="text-3xl mt-[4rem]">Links</div>
+            <ul className="mt-4">
+              <li className="text-xl">
+                <Link href="/">Portfolio</Link>
+              </li>
+              {/* <li className="text-xl">
+                <Link href="/">Blogs</Link>
+              </li> */}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
